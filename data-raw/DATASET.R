633b2e9e37dede832b21b133e9cc94cd00881ca8
Daniel D. Sjoberg <danield.sjoberg@gmail.com>
2021-09-30 08:42:16 -0400

    more updates

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,20 @@
 ## code to prepare `DATASET` dataset goes here
 
-usethis::use_data(DATASET, overwrite = TRUE)
+# adding a competing risks outcome to the gtsummary::trial dataset
+set.seed(1123)
+trial <-
+  gtsummary::trial %>%
+  dplyr::mutate(
+    death_cr = dplyr::case_when(
+      death == 0 ~ "censor",
+      stats::runif(dplyr::n()) < 0.5 ~ "death from cancer",
+      TRUE ~ "death other causes"
+    ) %>%
+      factor(),
+    .after = death,
+    death_cr_num = as.numeric(death_cr) - 1L
+  ) %>%
+  labelled::set_variable_labels(death_cr = "Death Status",
+                                death_cr_num = "Death Status (numeric)")
+
+usethis::use_data(trial, overwrite = TRUE)
