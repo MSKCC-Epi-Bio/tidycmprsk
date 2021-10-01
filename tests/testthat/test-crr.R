39333cf831da15fd9e72b99a1f86b7e6ba2cef8f
Fei <feit1@mskcc.org>
2021-10-01 15:12:22 -0400

    housekeeping to pass the check

--- tests/testthat/test-crr.R
+++ tests/testthat/test-crr.R
@@ -1,12 +1,6 @@
 test_that("crr() works", {
   expect_error(
     crr(crSurv(ttdeath, death_cr_num) ~ age, trial),
-    tidy(crr(crSurv(ttdeath, death_cr_num) ~ age, trial)),
-    predict(crr(crSurv(ttdeath, death_cr_num) ~ age, trial)),
-    glance(crr(crSurv(ttdeath, death_cr_num) ~ age, trial)),
-    augment(crr(crSurv(ttdeath, death_cr_num) ~ age, trial)),
-    model.frame(crr(crSurv(ttdeath, death_cr_num) ~ age, trial)),
-    model.matrix(crr(crSurv(ttdeath, death_cr_num) ~ age, trial)),
     NA
   )
 })
