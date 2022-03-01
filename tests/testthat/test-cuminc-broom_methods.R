3baaacd77b1f4fcd398302f1858c5ea4fbc88576
Daniel Sjoberg <danield.sjoberg@gmail.com>
2022-02-28 23:15:43 -0500

    Doc correction + add unit test (#68)
    * correcting documentation
    
    * adding checks for cum stats #39

--- tests/testthat/test-cuminc-broom_methods.R
+++ tests/testthat/test-cuminc-broom_methods.R
@@ -224,6 +224,16 @@ test_that("broom methods", {
     cuminc_tidy2 <- tidy(cuminc2, conf.int = FALSE, times = c(0, 12)),
     NA
   )
+  expect_equal(
+    cuminc_tidy2$cum.censor,
+    rep_len(0L, 8)
+  )
+  expect_equal(
+    cuminc_tidy2$cum.event,
+    c(0L, 3L, 0L, 6L, 0L, 9L, 0L, 5L)
+  )
+
+
 
   expect_error(
     cuminc_tidy1 <- tidy(cuminc1, conf.int = FALSE, times = c(0, 12)),
@@ -232,6 +242,14 @@ test_that("broom methods", {
   expect_equal(
     cuminc_tidy1$n.censor,
     rep_len(0L, 4)
+  )
+  expect_equal(
+    cuminc_tidy1$cum.censor,
+    rep_len(0L, 4)
+  )
+  expect_equal(
+    cuminc_tidy1$cum.event,
+    c(0L, 12L, 0L, 11L)
   )
 
   # testing tidy with problematic times
